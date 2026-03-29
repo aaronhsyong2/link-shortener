7b9a12f2802b010c0368f613e0542645e52a5cac
Aaron Yong <aaronhsyong2@gmail.com>
2026-03-29 12:27:26 +0800

    chore: remove Minitest tests

--- test/system/urls_test.rb
+++
@@ -1,6 +0,0 @@
-require "application_system_test_case"
-
-class UrlsTest < ApplicationSystemTestCase
-  # System tests will be implemented in Phase 6 (Testing)
-  # Skipping for now due to selenium-webdriver 4.41 / Ruby 3.3 incompatibility
-end
